04a3052ce16bb363d9ba22c973940925fc04ddb0
thanglegons <dthangnguyen19@gmail.com>
2018-09-27 15:50:47 +0700

    Update Code Style

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,12 +1,11 @@
-package com.company;
 
 import java.util.ArrayList;
 
 public class DictionaryCommandline {
-    public static void showAllwWords(DictionaryManagement dictionaryManagement){
+    public static void showAllwWords(DictionaryManagement dictionaryManagement) {
         System.out.println("No" + "\t" + "| English" + "\t" + "|Vietnamese");
-        for (Word temp: dictionaryManagement.getWordList()){
-            System.out.println((temp.getId()+1) + "\t" + temp.getWord_target() + "\t" + temp.getWord_explaint());
+        for (Word temp : dictionaryManagement.getWordList()) {
+            System.out.println((temp.getId() + 1) + "\t" + temp.getWord_target() + "\t" + temp.getWord_explaint());
         }
     }
 }
