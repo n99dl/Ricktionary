1f63c10312d11282ec59b07ffc616f7f1ab8baea
thanglegons <dthangnguyen19@gmail.com>
2018-09-27 16:03:05 +0700

    Fix Typo

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 
 public class DictionaryCommandline {
-    public static void showAllwWords(DictionaryManagement dictionaryManagement) {
+    public static void showAllWords(DictionaryManagement dictionaryManagement) {
         System.out.println("No" + "\t" + "| English" + "\t" + "|Vietnamese");
         for (Word temp : dictionaryManagement.getWordList()) {
             System.out.println((temp.getId() + 1) + "\t" + temp.getWord_target() + "\t" + temp.getWord_explaint());
