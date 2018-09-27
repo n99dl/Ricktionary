5f437942db8e07ead43f2f87bc81bb167420e667
thanglegons <dthangnguyen19@gmail.com>
2018-09-27 15:50:34 +0700

    Update Code Style

--- Dictionary.java
+++ Dictionary.java
@@ -1,11 +1,11 @@
-package com.company;
 
 import java.util.ArrayList;
 
 public class Dictionary {
     private Trie trie = new Trie();
     private ArrayList<Word> wordList = new ArrayList<Word>();
-    public void insertWord(Word word){
+
+    public void insertWord(Word word) {
         trie.insertToTree(word);
         wordList.add(word);
     }
@@ -13,12 +13,14 @@ public class Dictionary {
     public ArrayList<Word> getWordList() {
         return wordList;
     }
-    public Word getWord(int index){
+
+    public Word getWord(int index) {
         return wordList.get(index);
     }
-    public Word lookupWord(String word){
-        ArrayList<Integer> arrayListWord = trie.searchInTree(word,1);
-        if (arrayListWord.isEmpty()){
+
+    public Word lookupWord(String word) {
+        ArrayList<Integer> arrayListWord = trie.searchInTree(word, 1);
+        if (arrayListWord.isEmpty()) {
             return null;
         }
         return wordList.get(arrayListWord.get(0));
