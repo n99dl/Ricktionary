b2ab4a1b642eb49e7998459d5ada26c0d7eab3cc
thanglegons <dthangnguyen19@gmail.com>
2018-09-27 15:48:16 +0700

    Add SortWords

--- Word.java
+++ Word.java
@@ -1,4 +1,4 @@
-package com.company;
+import java.util.Comparator;
 
 public class Word {
     private String word_target, word_explaint;
@@ -33,7 +33,16 @@ public class Word {
     public void setId(int id) {
         this.id = id;
     }
-    public void print(){
+
+    public void print() {
         System.out.println(this.id + "   " + this.word_target + "  " + this.word_explaint);
     }
+}
+
+public class SortWords implements Comparator<Word>{
+    public int compare(Word a, Word b){
+        String wordA = a.getWord_target();
+        String wordB = b.getWord_target();
+        return wordA.compareTo(wordB);
+    }   
 }
