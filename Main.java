bf6e18b7da27c280f76a66d3ac48da50b45922a4
thanglegons <dthangnguyen19@gmail.com>
2018-09-27 16:17:20 +0700

    Update

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         DictionaryManagement dictionaryManagement = new DictionaryManagement();
         dictionaryManagement.insertFromFile();
-        DictionaryCommandline.showAllwWords(dictionaryManagement);
-        Word temp = new Word("Hello","Xin Chao", 1);
-        temp.print();
+        DictionaryCommandline.showAllWords(dictionaryManagement);
+        // Word temp = new Word("Hello","Xin Chao", 1);
+        // temp.print();
     }
 }
